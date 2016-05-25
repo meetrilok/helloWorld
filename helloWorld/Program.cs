17b955d19bf52d1e59db2943d6d3d3b585f270e6
Trilok Chowdary Maddipudi <mtrilokchowdary@ymail.com>
2016-05-25 07:06:14 +0530

    Writeline program

--- helloWorld/Program.cs
+++ helloWorld/Program.cs
@@ -10,6 +10,9 @@ namespace helloWorld
     {
         static void Main(string[] args)
         {
+            int a = 10;
+            Console.WriteLine("Welcome Interns {0} {1} {0}",a,++a);
+            Console.ReadLine();
         }
     }
 }
